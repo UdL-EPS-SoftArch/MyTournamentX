f93f4c115be899bcbad06f70f857aec09bb9b6e2
Francesc <elskater98@gmail.com>
2019-12-04 15:02:53 +0100

    Create Team -> post

--- src/app/team/team-create/team-create.component.ts
+++ src/app/team/team-create/team-create.component.ts
@@ -21,7 +21,7 @@ export class TeamCreateComponent implements OnInit {
 
 
   onSubmit():void{
-    this.teamService.create(this.team).subscribe((team: Team) => this.router.navigate(['/teams']));
+    this.teamService.create(this.team).subscribe((team: Team) => this.router.navigate(['/teams'])); // ruta API-post
   }
   
 }
