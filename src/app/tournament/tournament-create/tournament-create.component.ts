d9d715664880a82d941ec48df1f4f18534e49f56
Marc Masip <mmasipcoloma@gmail.com>
2019-12-10 18:08:17 +0100

    Changed name file and files tournament edit

--- src/app/tournament/tournament-create/tournament-create.component.ts
+++ src/app/tournament/tournament-create/tournament-create.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Injectable } from '@angular/core';
 import { Tournament } from '../tournament';
 import { Router } from '@angular/router';
 import { TournamentServiceService } from '../tournament.service';
-
+//Tournament Master service?
 @Component({
   selector: 'app-tournament-create',
   templateUrl: './tournament-create.component.html',
@@ -13,7 +13,7 @@ export class TournamentCreateComponent implements OnInit {
 
   public tournament: Tournament;
 
-  constructor( private router: Router,private tournamentService:TournamentServiceService) { }
+  constructor( private router: Router, private tournamentService:TournamentServiceService) { }
 
   ngOnInit() {
     this.tournament = new Tournament();
