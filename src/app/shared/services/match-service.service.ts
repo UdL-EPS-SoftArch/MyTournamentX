53181870904617a7e1ef4f01be5fc1b49841ef95
Toni Pacheco <toni.pafe@gmail.com>
2019-12-10 17:25:09 +0100

    Created basic service.

--- src/app/shared/services/match-service.service.ts
+++ src/app/shared/services/match-service.service.ts
@@ -1,6 +1,7 @@
 import {Injectable, Injector} from '@angular/core';
 import {RestService} from '@lagoshny/ngx-hal-client';
 import {Match} from '../models/match';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,5 +10,10 @@ export class MatchServiceService extends RestService<Match> {
 
   constructor(injector: Injector) {
     super(Match, 'matches', injector);
+  }
+
+  public findByRound(text: string): Observable<Match[]> {
+    const options: any = {params: [{key: 'text', value: text}]};
+    return this.search('findByRound', options);
   }
 }
