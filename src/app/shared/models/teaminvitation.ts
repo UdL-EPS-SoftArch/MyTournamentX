9df477f25d73849b770b66070661e3d5e6759203
viloc98 <viloc98@gmail.com>
2019-12-11 16:38:51 +0100

    TeamInvitation Detail Inizialitation

--- src/app/shared/models/teaminvitation.ts
+++ src/app/shared/models/teaminvitation.ts
@@ -1,8 +1,10 @@
 import {Resource} from '@lagoshny/ngx-hal-client';
+import {Team} from './team';
 
 export class TeamInvitation extends Resource {
   id: string;
   message: string;
+  team: Team;
   accepted: boolean;
   creationDate: Date;
 
