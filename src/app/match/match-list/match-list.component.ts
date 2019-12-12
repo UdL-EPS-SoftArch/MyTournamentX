6a858934903fd26bb248f861ba17d5b8767c82ff
raultds <rrj1@alumnes.udl.cat>
2019-12-12 18:00:38 +0100

    implmented match list component

--- src/app/match/match-list/match-list.component.ts
+++ src/app/match/match-list/match-list.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
-import { MatchServiceService } from '../../shared/services/match-service.service'
+import { MatchServiceService } from '../../shared/services/match-service.service';
 import { Match } from '../../shared/models/match';
-import { Round } from '../../shared/models/round';
+import { Team } from 'src/app/shared/models/team';
+import { Round } from 'src/app/shared/models/round';
 
 @Component({
   selector: 'app-match-list',
@@ -11,50 +12,31 @@ import { Round } from '../../shared/models/round';
   templateUrl: './match-list.component.html'
 })
 export class MatchListComponent implements OnInit {
+
     private matches: Match[];
-    private round: Round = new Round();
+    @Input() round: Round;
 
     constructor(private route: ActivatedRoute,
                 private router: Router,
-                //private round: Round,
                 private matchService: MatchServiceService) {
     }
 
   ngOnInit() {
-    this.round.uri = 'http://localhost:8080/rounds/1';
-
-
+    console.log('TEST-->', this.round);
     this.matchService.findByRound(this.round.uri).subscribe(
       matches => {
-        console.log(this.matches);
-        /*
-        matches.map(match => {
-          this.matchService.findById(match.uri).subscribe(
-            matchObject => {
-               this.matches.push(matchObject[0]);
-               console.log(matchObject);
-               console.log(this.matches);
-            }
-          );
-        });*/
-      });
+        this.matches = matches;
+        this.matches.map(match => {
+          if (match.hasWinner) {
+            match.getRelation(Team, 'winner')
+            .subscribe((team: Team) => {
+              if (match.hasWinner) {
+                match.winner = team;
+              }
+            });
+          }
+        });
+      }
+    );
   }
 }
-
-/*
-    const team1 = new Team();
-    team1.name = "Patata 1";
-
-    const team2 = new Team();
-    team2.name = "Patata 2";
-
-
-    this.round.numTeams = 2;
-    this.round.rivals = [team1, team2];
-
-    const match = this.matches[0];
-    match.round = this.round;
-    match.winner = this.round.rivals[0];
-
-    this.matches[1] = this.matches[0];
-*/
