e9d3105bc06ada7127fb234c6d1663bffae1e7f8
raultds <rrj1@alumnes.udl.cat>
2019-12-18 15:58:05 +0100

    implemented tournament service

--- src/app/shared/services/tournament-service.service.ts
+++ src/app/shared/services/tournament-service.service.ts
@@ -1,6 +1,7 @@
 import {Injectable, Injector} from '@angular/core';
 import {RestService} from '@lagoshny/ngx-hal-client';
 import {Tournament} from '../models/tournament';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -10,4 +11,16 @@ export class TournamentServiceService extends RestService<Tournament> {
   constructor(injector: Injector) {
     super(Tournament, 'tournaments', injector);
   }
+
+  public findTournamentByName(id: string): Observable<Tournament[]> {
+    const options: any = {params: [{key: 'name', value: id}]};
+    return this.search('findTournamentByName', options);
+  }
+
+  /*
+  public existsByName(id: string): Boolean {
+    const options: any = {params: [{key: 'name', value: id}]};
+    return this.search('existsByName', options);
+  }  
+  */
 }
