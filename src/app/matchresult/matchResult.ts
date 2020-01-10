0dfc87fa213a14d466016591d090f5ca3f77fde1
nietzche <havocf4u@gmail.com>
2020-01-10 16:05:14 +0100

    Try to make detailed view

---
+++ src/app/matchresult/matchResult.ts
@@ -0,0 +1,19 @@
+import { Resource } from '@lagoshny/ngx-hal-client';
+import { Player } from 'src/app/shared/models/player';
+import {Match} from "../shared/models/match";
+import {Team} from "../shared/models/team";
+
+
+
+export class MatchResult extends Resource {
+  id: string;
+  winner: Team; // name of the team
+  // ATRIBUTES
+  sender: Team;
+  match: Match;
+  description: string;
+  uri: string;
+  constructor() {
+    super();
+  }
+}
